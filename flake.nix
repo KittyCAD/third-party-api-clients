e353963146da0a49f8e219d819c182f03b3b9d96
Jess Frazelle <github@jessfraz.com>
2025-04-09 09:34:21 -0700

    remove submodule
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- flake.nix
+++ flake.nix
@@ -8,41 +8,48 @@
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      # Overlays enable you to customize the Nixpkgs attribute set
-      overlays = [
-        # Makes a `rust-bin` attribute available in Nixpkgs
-        (import rust-overlay)
-        # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
-        # create a Rust environment
-        (self: super: {
-          rustToolchain = super. rust-bin.stable.latest.default.override {
-            extensions = [ "rustfmt" "llvm-tools-preview" ];
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    # Overlays enable you to customize the Nixpkgs attribute set
+    overlays = [
+      # Makes a `rust-bin` attribute available in Nixpkgs
+      (import rust-overlay)
+      # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
+      # create a Rust environment
+      (self: super: {
+        rustToolchain = super.rust-bin.stable.latest.default.override {
+          extensions = ["rustfmt" "llvm-tools-preview"];
+        };
+      })
+    ];
+
+    # Systems supported
+    allSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      "aarch64-linux" # 64-bit ARM Linux
+      "x86_64-darwin" # 64-bit Intel macOS
+      "aarch64-darwin" # 64-bit ARM macOS
+    ];
+
+    # Helper to provide system-specific attributes
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit overlays system;
+            config.allowBroken = true;
           };
-        })
-      ];
-
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-
-    in
-    {
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = (with pkgs; [
+        });
+  in {
+    # Development environment output
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        # The Nix packages provided in the environment
+        packages =
+          (with pkgs; [
             # The package provided by our custom overlay. Includes cargo, Clippy, cargo-fmt,
             # rustdoc, rustfmt, and other tools.
             rustToolchain
@@ -52,15 +59,16 @@
 
             # dependencies for api-deux
             just
-          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
-            libiconv 
+          ])
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+            libiconv
             darwin.apple_sdk.frameworks.Security
           ]);
 
-          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-          TARGET_CC = "${pkgs.stdenv.cc}/bin/${pkgs.stdenv.cc.targetPrefix}cc";
-          RUSTFMT = "${pkgs.rust-bin.nightly.latest.rustfmt}/bin/rustfmt";
-        };
-      });
-    };
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        TARGET_CC = "${pkgs.stdenv.cc}/bin/${pkgs.stdenv.cc.targetPrefix}cc";
+        RUSTFMT = "${pkgs.rust-bin.nightly.latest.rustfmt}/bin/rustfmt";
+      };
+    });
+  };
 }
